eadcd0eb2c81d2408fd94800608e3f16efc1c041
Satheesh Reddy <sathish.reddy48@gmail.com>
2019-01-06 22:29:05 +0530

    changes by satheesh

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<int> setofnos = new HashSet<int> { 1, 2 };
+            setofnos.Add(3);
+            setofnos.Add(2);
+            foreach (var el in setofnos)
+                Console.WriteLine(el);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PartialInvocation/PartialInvocation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialInvocation
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //partial invocation method
+            Func<int, int, int> AddSome = (valueToAdd, value) => value + valueToAdd;
+
+            SomeClass someClass = new SomeClass(2);
+            Console.WriteLine(someClass.AddSome(5));
+
+            // partial invocation
+            var result = AddSome(2, 1);
+           // var result2 = AddSome(2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+      
+    }
+    public sealed class SomeClass
+    {
+        public readonly int valueToAdd;
+        public SomeClass(int valueToAdd)
+        {
+            this.valueToAdd = valueToAdd;
+           
+        }
+        public int AddSome(int value) => value + valueToAdd;
+    }
+}
